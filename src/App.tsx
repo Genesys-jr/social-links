39b52e7ec7dd4663dc346c5131cdd22a5b005945
Amon Daniel <amon.daniel.91@gmail.com>
2025-07-18 14:33:49 +0000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import "./index.css";
 
 function App() {
   return (
-    <section className="bg-neutral-900 min-h-screen flex items-center justify-center">
-      <div className="bg-neutral-800 flex flex-col justify-center w-full max-w-md mx-auto h-auto rounded-2xl p-6">
+    <section className="bg-neutral-900 h-screen flex items-center justify-center overflow-hidden">
+      <div className="bg-neutral-800 flex flex-col justify-center w-full max-w-md mx-auto rounded-2xl p-6">
         <Profile />
         <Socials />
       </div>
