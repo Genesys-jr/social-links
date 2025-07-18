7de74e4749eaad999f47786320d2a733849f74b3
Amon Daniel <amon.daniel.91@gmail.com>
2025-07-18 14:26:53 +0000

    fixed responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 function App() {
   return (
     <section className="bg-neutral-900 min-h-screen flex items-center justify-center">
-      <div className="bg-neutral-800 flex flex-col justify-center mx-auto min-w-lg h-3/4 rounded-2xl ">
+      <div className="bg-neutral-800 flex flex-col justify-center w-full max-w-md mx-auto h-auto rounded-2xl p-6">
         <Profile />
         <Socials />
       </div>
